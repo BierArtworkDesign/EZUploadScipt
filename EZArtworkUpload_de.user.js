e6580e9e2e0d88eca6e6272cf4512d6ee1f11749
BierArtworkDesign <40516882+BierArtworkDesign@users.noreply.github.com>
2018-06-24 02:35:18 +0200

    Update EZArtworkUpload_de.user.js

--- EZArtworkUpload_de.user.js
+++ EZArtworkUpload_de.user.js
@@ -2,7 +2,7 @@
 // @name         Steam Long Artwork EZ Upload
 // @namespace    https://steamcommunity.com/id/chris_nbg/
 // @version      1.0de
-// @description  Fügt auf der Artwork Upload Seite eine Schaltfläche hinzu, die das Hochladen vereinfacht.
+// @description  Fügt auf der Artwork Upload Seite eine Schaltfläche hinzu, die das Hochladen von langen Artworks vereinfacht.
 // @author       Bier
 // @match        https://steamcommunity.com/sharedfiles/edititem/767/3/
 // @grant        none
@@ -13,15 +13,15 @@
     var noImageBier = 0;
     var noRightsBier = 0;
     //Hinweis dass das Script aktiv ist
-    $J('.pageTitle').after('<div class="pageTitle bier_script_activated" style="text-align:right;color:#9EEF11">Bier\'s EZ Upload Script activated!</div>');
+    $J('.pageTitle').after('<div class="pageTitle bier_script_activated" style="text-align:right;color:#9EEF11">Bier\'s EZ Upload Script ist aktiviert!</div>');
     //Farbanimation für Warnungen
-    $J('head').after('<style>@keyframes colorchange{0%   {background: black;}12%   {background: #962B19;}25%  {background: black;}37%  {background : #962B19;}50%  {background: black;}62%  {background: #962B19;}75%  {background: black;}87% {background: #962B19;}100% {background: #black;}}@-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */{0%   {background: black;}25%  {background: #962B19;}37%  {background : black;}50%  {background: #962B19;}75%  {background: black;}87% {background: #962B19;}100% {background: #black;}}</style>');
+    $J('head').after('<style>@keyframes colorchange{0%   {background: black;}12%   {background: #e20b00;}25%  {background: black;}37%  {background : #e20b00;}50%  {background: black;}62%  {background: #e20b00;}75%  {background: black;}87% {background: #e20b00;}100% {background: #black;}}@-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */{0%   {background: black;}25%  {background: #e20b00;}37%  {background : black;}50%  {background: #e20b00;}75%  {background: black;}87% {background: #e20b00;}100% {background: #black;}}</style>');
     //Gibt automatisch einen Titel ein
     $J('#title').val('Artwork by Bier');
     //Ändert den Hintergrund der Sichtbarkeitsoptionen
     $J('.visiblityOptionsDesc').css({ background: "#c17800",color:"#FFFFFF" });
     //Fügt die besagte Schaltfläche hinzu, unten zwischen Abbrechen und Speichern 
-    $J('.cancelButton').after('<div class="btnv6_white_transparent btn_medium bier_upload_button" id="bier_upload_button" style="margin-right:5px"><span>Upload as Long Artwork</span></div>');
+    $J('.cancelButton').after('<div class="btnv6_white_transparent btn_medium bier_upload_button" id="bier_upload_button" style="margin-right:5px"><span>Hochladen als langes Artwork</span></div>');
 
     //Gibt an was passiert wenn auf die neue Schaltfläche geklickt wird
     $J('#bier_upload_button').click(function(){
@@ -36,12 +36,12 @@
             $J('html, body').animate({
                 scrollTop: $J("#PreviewImage").offset().top
             }, 400);
-            //Wenn diese Warnung ein zweites Mal ausgelöst wird erscheint ein PopUp
+            //Wenn diese Warnung ein zweites Mal ausgelöst wird erscheint ein helfendes PopUp
             if(noImageBier === 0){
                 noImageBier = 1;
                 return false;
             }
-		}
+	} else
         //Falls der Haken bei Berechtigungen und Eigentümerschaft nicht gesetzt wurde, scrollt die Seite zum Kasten und blinkt für 6 Sekunden rot/schwarz
         if( !$('agree_terms').checked ){
             $J("label[for='agree_terms']").css({ "animation": "colorchange 6s",
@@ -49,7 +49,7 @@
             $J('html, body').animate({
                 scrollTop: $J("#agree_terms").offset().top
             }, 400);
-            //Wenn diese Warnung ein zweites Mal ausgelöst wird erscheint ein PopUp
+            //Wenn diese Warnung ein zweites Mal ausgelöst wird erscheint ein helfendes PopUp
             if(noRightsBier === 0){
                 noRightsBier = 1;
                 return false;
